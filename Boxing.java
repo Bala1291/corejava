9a71390d676e21f05a5dab3811a90e4162a65bf3
Bala1291 <36030984+Bala1291@users.noreply.github.com>
2018-02-16 12:25:14 +0530

    Wrapperclasses(boxing,unboxing)
    Wrapperclasses(boxing,unboxing)

---
+++ Boxing.java
@@ -0,0 +1,9 @@
+
+public class Boxing {
+	public static void main(String[] args) {
+		byte b=10;
+		Byte B=new Byte(b);
+		System.out.println(B);
+	}
+
+}
---
+++ Unboxing.java
@@ -0,0 +1,9 @@
+
+public class Unboxing {
+	public static void main(String[] args) {
+		Byte B=new Byte((byte)10);
+		Byte b=B.byteValue();
+		System.out.println(B);
+		
+	}
+}
