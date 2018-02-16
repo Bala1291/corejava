e631a309af5aad5ea05248eb3540de1b659042e6
Bala1291 <36030984+Bala1291@users.noreply.github.com>
2018-02-16 12:04:38 +0530

    Hashmap,hashtable
    Hashmap,hashtable

---
+++ HashMapDemo.java
@@ -0,0 +1,18 @@
+package com.Map;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+public static void main(String[] args) {
+HashMap<String,Integer> hm=new HashMap<String,Integer>();
+hm.put("prasanna",1);
+hm.put("LAXMI",2);
+hm.put("SAI",1);
+hm.put(null,1);
+
+System.out.println(hm);
+
+
+	}
+
+}
---
+++ HashTableDemo.java
@@ -0,0 +1,14 @@
+package com.Map;
+
+import java.util.Hashtable;
+public class HashTableDemo {
+public static void main(String[] args) {
+Hashtable<Integer,String> hm=new Hashtable<Integer,String>();  
+		hm.put(100,"prasanna");  
+		hm.put(102,"java");  
+		hm.put(101,"laxmi");  
+		hm.put(103,"sampada");  
+	System.out.println(hm);
+	}
+
+}
