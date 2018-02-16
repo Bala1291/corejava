6dbcc050c9f7527ecb0a997ec54c4e5038f2f026
Bala1291 <36030984+Bala1291@users.noreply.github.com>
2018-02-16 12:09:10 +0530

    SET Example
    SET Example

--- Test1.java
+++ Test1.java
@@ -1,58 +1,73 @@
+package com.SET;
 
+import java.util.HashSet;
+import java.util.Iterator;
 
-package com.prasanna;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-class Employee {
-    int empId;
+class Student
+{
     String name;
-    float salary;
-Employee(int empId, String name, float salary) {
-        this.empId = empId;
+ 
+    int rollNo;
+ 
+    String department;
+ 
+    public Student(String name, int rollNo, String department)
+    {
         this.name = name;
-        this.salary = salary;
+ 
+        this.rollNo = rollNo;
+ 
+        this.department = department;
+    }
+ 
+    @Override
+    public int hashCode()
+    {
+        return rollNo;
+    }
+ 
+    @Override
+    public boolean equals(Object obj)
+    {
+        Student student = (Student) obj;
+ 
+        return (rollNo == student.rollNo);
+    }
+ 
+    @Override
+    public String toString()
+    {
+        return rollNo+", "+name+", "+department;
+    }
 }
-@Override
-public String toString() {
-    return "Employee [empId=" + empId + ", name=" + name + ", salary=" + salary
-            + "]";
-}
-}
-class EmployeeIdComparetor implements Comparator {
-    public int compare(Object o1, Object o2) {
-        Employee e1 = (Employee) o1;
-        Employee e2 = (Employee) o2;
-        if (e1.empId == e2.empId) {
-            return 0;
-        } else if (e1.empId > e2.empId) {
-            return 1;
-        } else {
-            return -1;
+ 
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        HashSet<Student> set = new HashSet<Student>();
+ 
+        //Adding elements to HashSet
+ 
+        set.add(new Student("Avinash", 121, "ECE"));
+ 
+        set.add(new Student("Bharat", 101, "EEE"));
+ 
+        set.add(new Student("Bharat", 301, "IT"));
+ 
+        set.add(new Student("Amit", 301, "IT"));           //duplicate element
+ 
+        set.add(new Student("Bhavya", 872, "ECE"));
+ 
+        //Iterating through HashSet
+ 
+        Iterator<Student> it = set.iterator();
+ 
+        while (it.hasNext())
+        {
+            Student student = (Student) it.next();
+ 
+            System.out.println(student);
         }
-    }
-}
-class EmployeeNameComparator implements Comparator{
-    public int compare(Object o1, Object o2) {
-        Employee e1=(Employee)o1;
-        Employee e2=(Employee)o2;
-        return e1.name.compareTo(e2.name);
-    }
-}
-public class Test1 {
-    public static void main(String[] args) {
-        Employee e1 = new Employee(45, "ram", 6780);
-     
-        Employee e2 = new Employee(23, "yasir", 8767);
-        Employee e3 = new Employee(90, "balu", 78);
-        ArrayList<Employee> al = new ArrayList<Employee>();
-        al.add(e1);
-        al.add(e2);
-        al.add(e3);
-        System.out.println(al);
-        Collections.sort(al,new EmployeeIdComparetor());
-        System.out.println(al);
-        Collections.sort(al,new EmployeeNameComparator());
-        System.out.println(al);
     }
 }
