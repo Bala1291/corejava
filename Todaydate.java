c07fbe948ea8c9670231d69e9282f4d59f75e026
Bala1291 <36030984+Bala1291@users.noreply.github.com>
2018-02-16 12:00:56 +0530

    Todaydate
    Todaydate

---
+++ Todaydate.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+
+public class Todaydate {
+	public static void main(String[] args) {
+		
+		LocalDate today = LocalDate.now();
+		System.out.println("Today's Local date : " + today);
+		
+	}
+	
+
+}
