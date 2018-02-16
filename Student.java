8a8d8ea068210d5072906ea8241a3d62cf314146
Bala1291 <36030984+Bala1291@users.noreply.github.com>
2018-02-16 12:24:10 +0530

    toString
    toString

--- Student.java
+++ Student.java
@@ -1,14 +1,73 @@
-package com.pras;
-import java.io.Serializable;
-public class Student implements Serializable
-{
-	int id;
+package com.prasanna;
+
+class Student {
+	int rollno;
 	String name;
+	String city;
 
-Student(int id,String name)
-{
-	this.id=id;
-	this.name=name;
-}
-}
+	Student(int rollno, String name, String city) {
+		this.rollno = rollno;
+		this.name = name;
+		this.city = city;
+	}
+	
 
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + rollno;
+		return result;
+	}
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (rollno != other.rollno)
+			return false;
+		return true;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", name=" + name + ", city="
+				+ city + "]";
+	}
+
+
+
+
+	public static void main(String args[]) {
+		Student s1 = new Student(101, "Raj", "lucknow");
+		Student s2 = new Student(102, "Vijay", "ghaziabad");
+
+		System.out.println(s1);// compiler writes here s1.toString()
+		System.out.println(s2);// compiler writes here s2.toString()
+	}
+}
