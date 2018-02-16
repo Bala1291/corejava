93dd046899a74ce95746bd46bc7d51e6dffd8f45
Bala1291 <36030984+Bala1291@users.noreply.github.com>
2018-02-16 12:46:05 +0530

    Update HashMapDemo.java

--- HashMapDemo.java
+++ HashMapDemo.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 public class HashMapDemo {
 public static void main(String[] args) {
 HashMap<String,Integer> hm=new HashMap<String,Integer>();
-hm.put("prasanna",1);
-hm.put("LAXMI",2);
+hm.put("bala",1);
+hm.put("kammari",2);
 hm.put("SAI",1);
 hm.put(null,1);
 
