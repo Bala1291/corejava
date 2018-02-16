ad9060d0306ea7e7877f9a9f1ad33bebcc03c837
Bala1291 <36030984+Bala1291@users.noreply.github.com>
2018-02-16 11:58:15 +0530

    AscendingOrder
    AscendingOrder

---
+++ AscendingOrder.java
@@ -0,0 +1,44 @@
+package com.prasanna;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AscendingOrder {
+
+    public static void main(String[] args) throws IOException {
+
+        ObjectInputStream input = new ObjectInputStream(new FileInputStream("words.txt"));
+
+ 
+
+        Map<String, Integer> hashMap = new HashMap<String, Integer>();
+
+ 
+
+        // code to read the words in the text file into the hashMap
+
+        hashMap.put(input.readObject(), 1);
+
+        hashMap.put(input.readUTF(), 2);
+
+        hashMap.put(input.readUTF(), 3);
+
+        hashMap.put(input.readUTF(), 4);
+
+        input.close();
+
+        Map<String, Integer> treeMap = new TreeMap<String,  Integer>(hashMap);
+
+        System.out.println("Display entries in ascending order of key");
+
+        System.out.println(treeMap + "\n");
+
+ 
+
+    }
+
+}
