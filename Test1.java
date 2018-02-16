8302ca80119701ad4f99062e7f63809b697a7ac7
Bala1291 <36030984+Bala1291@users.noreply.github.com>
2018-02-16 11:46:35 +0530

    ComparatorSorting
    ComparatorSorting

--- Test1.java
+++ Test1.java
@@ -1,44 +1,58 @@
 
 
-import java.util.SortedSet;
-import java.util.TreeSet;
-
-class Employee implements Comparable {
+package com.prasanna;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+class Employee {
     int empId;
     String name;
-
-    Employee(int empId, String name) {
+    float salary;
+Employee(int empId, String name, float salary) {
         this.empId = empId;
         this.name = name;
-    }
-
-    public String toString() {
-        return empId + "-" + name;
-    }
-
-    public int compareTo(Object obj) {
-        Employee e = (Employee) obj;
-        
-        if (this.empId == e.empId) {
+        this.salary = salary;
+}
+@Override
+public String toString() {
+    return "Employee [empId=" + empId + ", name=" + name + ", salary=" + salary
+            + "]";
+}
+}
+class EmployeeIdComparetor implements Comparator {
+    public int compare(Object o1, Object o2) {
+        Employee e1 = (Employee) o1;
+        Employee e2 = (Employee) o2;
+        if (e1.empId == e2.empId) {
             return 0;
-        } else if (this.empId > e.empId) {
+        } else if (e1.empId > e2.empId) {
             return 1;
         } else {
             return -1;
         }
     }
 }
+class EmployeeNameComparator implements Comparator{
+    public int compare(Object o1, Object o2) {
+        Employee e1=(Employee)o1;
+        Employee e2=(Employee)o2;
+        return e1.name.compareTo(e2.name);
+    }
+}
 public class Test1 {
     public static void main(String[] args) {
-        SortedSet st = new TreeSet();
-        Employee e1 = new Employee(12, "abc");
-        Employee e2 = new Employee(4, "hhh");
-        Employee e3 = new Employee(1, "ram");
-        st.add(e1);
-        st.add(e2);
-        st.add(e3);
-        System.out.println(st);
-
+        Employee e1 = new Employee(45, "ram", 6780);
+     
+        Employee e2 = new Employee(23, "yasir", 8767);
+        Employee e3 = new Employee(90, "balu", 78);
+        ArrayList<Employee> al = new ArrayList<Employee>();
+        al.add(e1);
+        al.add(e2);
+        al.add(e3);
+        System.out.println(al);
+        Collections.sort(al,new EmployeeIdComparetor());
+        System.out.println(al);
+        Collections.sort(al,new EmployeeNameComparator());
+        System.out.println(al);
     }
-
 }
